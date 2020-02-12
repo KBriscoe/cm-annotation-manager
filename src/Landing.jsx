99d654b27887fecab1179ff7f51fe14d03dfaa67
Kyler Briscoe <Kyler.Briscoe@callminerhq.callminer.net>
2020-02-12 10:11:22 -0500

    Add module link to nav

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
+import { userService, authenticationService } from '@/_services';
+
 class Landing extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            currentUser: authenticationService.currentUserValue,
+            users: null
+        };   
+    }
+
+    componentDidMount() {
+        userService.getAll().then(users => this.setState({ users }));
     }
 
     render() {
         return(
             <div className='LF-login-ctn'>
-            
+            <div>{this.state.currentUser.firstName}</div>
             </div>
         )
         }
