2ffea93fbde1fcbd6aaffa2413ae980a321fe04a
Kyler Briscoe <Kyler.Briscoe@gmail.com>
2020-03-30 14:11:38 -0400

    Add project view link

--- src/App.js
+++ src/App.js
@@ -35,13 +35,15 @@ class App extends React.Component {
                         <nav className="navbar navbar-expand navbar-dark bg-dark">
                             <div className="navbar-nav">
                                 <Link to="/" className="nav-item nav-link">Home</Link>
-                                <Link to="/projectcreation" className="nav-item nav-link">Linker</Link>
+                                <Link to="/projectcreation" className="nav-item nav-link">Project Creation</Link>
+                                <Link to="/projectview" className="nav-item nav-link">Project View</Link>
                                 <a onClick={this.logout} className="nav-item nav-link">Logout</a>
                             </div>
                         </nav>
                     }
                     <PrivateRoute exact path="/" component={Landing} />
                     <PrivateRoute path="/projectcreation" component={ProjectCreation} />
+                    <PrivateRoute path="/projectview" component={ProjectView} />
                     <Route path="/login" component={LoginPage} />
                 </div>
             </Router>
