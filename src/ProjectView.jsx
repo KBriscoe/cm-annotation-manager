10ad760ff81ddf79755042284fb7d8247fb91005
unknown <tmarlow98@gmail.com>
2020-04-01 21:53:43 -0400

    trying to fix value errors

--- src/ProjectView.jsx
+++ src/ProjectView.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import { userService, authenticationService } from '@/_services';
 
 class ProjectView extends Component {
